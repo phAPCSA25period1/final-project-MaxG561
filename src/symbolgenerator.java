3f7a7bc041b58d958ce3c025e79c26da4f7bb777
MaxG561 <trainmaster03@yahoo.com>
2026-04-13 16:45:12 +0000

    added symbol generator class and tested that it worked by creating test method

---
+++ src/symbolgenerator.java
@@ -0,0 +1,8 @@
+public class symbolgenerator {
+public static void main(String[] args) throws Exception {
+    }
+    public static void hello() {
+        System.out.println("HIIIIII");
+    }
+}
+
