60307f2969172443fb2b523bc9d94035fb392e13
MaxG561 <trainmaster03@yahoo.com>
2026-04-13 17:04:46 +0000

    added symbol generator.java and added imports, need to update import in symbol generator to be * not scanner

--- src/mainhub.java
+++ src/mainhub.java
@@ -1,6 +1,12 @@
+import java.util.*;
 public class mainhub {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello welcome to the minigame hub area please choose a game to play");
+        System.out.println("1. symbol generator");
+        System.out.println("2. 21");
         symbolgenerator.hello();
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println(Scanner.nextLine());
+
     }
 }
--- src/symbolgenerator.java
+++ src/symbolgenerator.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 public class symbolgenerator {
 public static void main(String[] args) throws Exception {
+    Scanner Scanner =new Scanner(System.in)
     }
     public static void hello() {
         System.out.println("HIIIIII");
     }
+    public static void scannertest(){
+
+    }
 }
+
 
